5cb7f2acf4b81ffdaa7fac54063429ab62c3e5a0
Heeseung Lee <final.lee@thecorporation.co.kr>
2020-01-08 20:44:39 +0900

    chore: 프로젝트 초기 부분 수정 및 필요없는 부분 제거

--- development/src/routes/index.js
+++ development/src/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Yahoo' })
+router.get('/', (req, res, next) => {
+  return res.json({ message: 'pong' })
 })
 
 module.exports = router
