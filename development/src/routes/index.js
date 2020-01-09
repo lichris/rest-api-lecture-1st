53261de8eb05784bb424bef301aeb9415d34cb48
Heeseung Lee <final.lee@thecorporation.co.kr>
2020-01-09 20:11:21 +0900

    no message

--- development/src/routes/index.js
+++ development/src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  return res.json({ message: 'pong' })
+  return res.json({ message: 'pong pong' })
 })
 
 module.exports = router
