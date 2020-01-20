17637f92699fe84b08c11c01bcbc77db7d4f92ec
Heeseung Lee <final.lee@thecorporation.co.kr>
2020-01-20 12:38:22 +0900

    fix: docker-compose 가 윈도우에서도 정상 작동할 수 있도록 수정

--- development/node/prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /home/app
-npm i
-npm start
--- development/src/routes/index.js
+++ development/src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  return res.json({ message: 'pong pong' })
+  return res.json({ message: '와 진짜 신기하다!' })
 })
 
 module.exports = router
