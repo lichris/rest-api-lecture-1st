dc52bdb724a41290014c792cd1b4d40c4e5e3c47
Heeseung Lee <final.lee@thecorporation.co.kr>
2020-04-11 16:21:14 +0900

    text: 오타 수정

--- development/src/routes/v1/index.js
+++ development/src/routes/v1/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  return res.json({ message: 'v1 pong' })
+  return res.json({ message: 'v1 ping' })
 })
 
 router.use('/auth', require('./auth.route'))
