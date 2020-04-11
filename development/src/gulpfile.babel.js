53e0afbd698b9784d1857098fbe83c4b2dfec2a9
Heeseung Lee <final.lee@thecorporation.co.kr>
2020-04-11 14:40:17 +0900

    chore: 배포전 확인 백업

--- development/src/gulpfile.babel.js
+++ development/src/gulpfile.babel.js
@@ -10,14 +10,17 @@ const envPath = './.env'
 
 const paths = {
   js: [
+    'bin/www',
     './**/*.js',
-    '!node_modules/**'
+    '!node_modules/**',
+    '!coverage/**'
   ],
   nonJs: [
     './package-lock.json',
-    './package.json'
+    './package.json',
+    './.sequelizerc',
+    './**/*.ejs'
   ]
-  //tests: './tests/*.js',
 }
 
 // Clean up dist and coverage directory
@@ -25,20 +28,21 @@ gulp.task(
   'clean',
   async () => await del.sync(
     [
-      '../dist/bin',
-      '../dist/config',
-      '../dist/controllers',
-      '../dist/emails',
-      '../dist/messages',
-      '../dist/middlewares',
-      '../dist/migrations',
-      '../dist/models',
-      '../dist/routes',
-      '../dist/tests',
-      '../dist/utils',
-      '../dist/*.*',
-      '../dist/.*',
-      '!../dist',
+      '../../production/bin',
+      '../../production/config',
+      '../../production/controllers',
+      '../../production/coverage',
+      '../../production/middlewares',
+      '../../production/migrations',
+      '../../production/models',
+      '../../production/repos',
+      '../../production/routes',
+      '../../production/tests',
+      '../../production/utils',
+      '../../production/validations',
+      '../../production/*.*',
+      '../../production/.*',
+      '!../../production',
       '!coverage'
     ],
     { force: true }
@@ -50,20 +54,20 @@ gulp.task('copy-env', () => {
   return gulp
     .src(envPath)
     .pipe(replace('development', 'production'))
-    .pipe(gulp.dest('../dist'))
+    .pipe(gulp.dest('../../production'))
 })
 
 // Copy .env with development setting
 gulp.task('copy-env-dev', () => {
-  return gulp.src(envPath).pipe(gulp.dest('../dist'))
+  return gulp.src(envPath).pipe(gulp.dest('../../production'))
 })
 
 // Copy non-js files to dist
 gulp.task('copy', () => {
   return gulp
     .src(paths.nonJs)
-    .pipe(plugins.newer('../dist'))
-    .pipe(gulp.dest('../dist'))
+    .pipe(plugins.newer('../../production'))
+    .pipe(gulp.dest('../../production'))
 })
 
 // Compile ES6 to ES5 and copy to dist
@@ -73,16 +77,17 @@ gulp.task('babel', () => {
       ...paths.js,
       '!gulpfile.js'
     ], { base: '.' })
-    .pipe(plugins.newer('../dist'))
+    .pipe(plugins.newer('../../production'))
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.babel())
     .pipe(
       plugins.sourcemaps.write('.', {
         includeContent: false,
-        sourceRoot (file) {
+        // eslint-disable-next-line space-before-function-paren
+        sourceRoot(file) {
           return path.relative(file.path, __dirname)
         }
       })
     )
-    .pipe(gulp.dest('../dist'))
+    .pipe(gulp.dest('../../production'))
 })
